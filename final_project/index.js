c8c0af2ff40d700898d34298db7d5cc55baf3c89
Joseph Villalba <joseph.villalba@j2lc.com>
2023-12-26 22:58:44 +0100

    final

--- final_project/index.js
+++ final_project/index.js
@@ -1,22 +1,44 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const session = require('express-session')
-const customer_routes = require('./router/auth_users.js').authenticated;
-const genl_routes = require('./router/general.js').general;
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const session = require("express-session");
+const customer_routes = require("./router/auth_users.js").authenticated;
+const genl_routes = require("./router/general.js").general;
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use(
+  "/customer",
+  session({
+    secret: "fingerprint_customer",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
-});
- 
-const PORT =5000;
+app.use("/customer/auth/*", function auth(req, res, next) {
+    // Check if the session and authorization data exist
+    if (req.session && req.session.authorization && req.session.authorization.accessToken) {
+      // Optionally, verify the access token for extra security
+      try {
+        jwt.verify(req.session.authorization.accessToken, "access");
+  
+        // If verification is successful, proceed to the next middleware/route handler
+        next();
+      } catch (err) {
+        // If the token is invalid or expired, send an unauthorized response
+        return res.status(401).json({ message: "Unauthorized access - Invalid token" });
+      }
+    } else {
+      // If session or authorization data is missing, send an unauthorized response
+      return res.status(401).json({ message: "Unauthorized access - Please log in" });
+    }
+  });
+
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log("Server is running"));
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,43 +1,102 @@
-const express = require('express');
+const express = require("express");
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const axios = require('axios');
 
 
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.post("/register", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  console.log(username);
+  if (username && password) {
+    if (!isValid(username)) {
+      users.push({ username: username, password: password });
+      return res
+        .status(200)
+        .json({ message: "User successfully registred. Now you can login" });
+    } else {
+      return res.status(404).json({ message: "User already exists!" });
+    }
+  }
+  return res.status(404).json({ message: "Unable to register user." });
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get("/", async function (req, res) {
+  try {
+    const response = await axios.get('http://localhost:5000/books');
+    return res.status(200).json(response.data);
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+
+
+// Get the book list available in the shop
+public_users.get("/books", function (req, res) {
+  return res.status(200).json(books);
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
-  
-// Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get("/isbn/:isbn", async function (req, res) {
+  const isbn = req.params.isbn;
+  try {
+    const response = await axios.get('http://localhost:5000/');
+    const book = response.data[isbn];
+    if (book) {
+      return res.status(200).json(book);
+    } else {
+      return res.status(404).json({ message: "Book not found" });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
 });
+
+
+// Get book details based on author
+public_users.get("/author/:author", async function (req, res) {
+  const authorName = req.params.author;
+  try {
+    const response = await axios.get('http://localhost:5000/');
+    const filteredBooks = Object.values(response.data).filter(book => book.author === authorName);
+    return res.status(200).json(filteredBooks);
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
+});
+
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get("/title/:title", async function (req, res) {
+  const titleName = req.params.title;
+  try {
+    const response = await axios.get('http://localhost:5000/');
+    const filteredBooks = Object.values(response.data).filter(book => book.title === titleName);
+    return res.status(200).json(filteredBooks);
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
 });
 
+
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get("/isbn/:isbn", async function (req, res) {
+  const isbn = req.params.isbn;
+  try {
+    const response = await axios.get(`http://localhost:5000/${isbn}`);
+    return res.status(200).json(response.data);
+  } catch (error) {
+    if (error.response && error.response.status === 404) {
+      return res.status(404).json({ message: "Book not found" });
+    } else {
+      return res.status(500).json({ message: "Internal server error" });
+    }
+  }
 });
+
 
 module.exports.general = public_users;
